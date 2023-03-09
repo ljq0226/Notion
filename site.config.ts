86aac9f48a067a6b04b26a2adfb5a093dd83a174
ljq0226 <82759390+ljq0226@users.noreply.github.com>
2023-03-09 16:55:23 +0800

    Update site.config.ts
    chore: change pageID

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '603cd2559b2a47f5aefd4da1c271bb3d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
